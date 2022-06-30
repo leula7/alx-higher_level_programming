98f29dfd79c4648e0ff8a72491312046ea30fc52
leula7 <75134713+leula7@users.noreply.github.com>
2022-06-30 22:12:21 +0300

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-args.py
-# Brennan D Baraban <375@holbertonschool.com>
+# Leul K
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i, sys.argv[i]))
